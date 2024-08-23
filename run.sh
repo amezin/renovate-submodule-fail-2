2f23c49fee11ca991f393c3365bc0f31b7287d77
Aleksandr Mezin <mezin.alexander@gmail.com>
2024-08-23 13:15:42 +0300

    Enable repository cache

--- run.sh
+++ run.sh
@@ -7,4 +7,5 @@ pnpm -C renovate install
 exec pnpm -C renovate start \
     "--base-dir=${PWD}/basedir" \
     --report-type=logging \
+    --repository-cache=enabled \
     amezin/renovate-submodule-fail-2
