34222fcce872f49483cf38c6992dfa05b278d54b
Aleksandr Mezin <mezin.alexander@gmail.com>
2024-08-23 11:49:39 +0300

    Disable persist-repo-data

--- run.sh
+++ run.sh
@@ -6,6 +6,5 @@ pnpm -C renovate install
 
 exec pnpm -C renovate start \
     "--base-dir=${PWD}/basedir" \
-    --persist-repo-data=true \
     --report-type=logging \
     amezin/renovate-submodule-fail-2
